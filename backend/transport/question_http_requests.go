3b4172b20573f6a422fd9947a770ffce3ef65630
rertyy <107781565+rertyy@users.noreply.github.com>
2024-10-04 23:31:19 +0800

    Add /health call and other minor changes

--- backend/transport/question_http_requests.go
+++ backend/transport/question_http_requests.go
@@ -17,16 +17,17 @@ func SetAllEndpoints(router *gin.Engine, db *database.QuestionDB, logger *common
 	router.GET("/questions/solve/:id", GetQuestionWithLogger(db, logger))
 	router.DELETE("/questions/delete/:id", DeleteQuestionWithLogger(db, logger))
 	router.PUT("/questions/replace/:id", ReplaceQuestionWithLogger(db, logger))
+	router.GET("/health", HealthCheck(logger))
 }
 
-//enable CORS for the frontend
+// enable CORS for the frontend
 func SetCors(router *gin.Engine) {
 	router.Use(cors.New(cors.Config{
-		AllowOrigins: []string{"http://localhost:3000"},
-		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders: []string{"Origin", "Content-Type", "Content-Length", "Authorization"},
-		ExposeHeaders: []string{"Content-Length"},
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Content-Length", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
-		MaxAge: 2 * time.Minute,
+		MaxAge:           2 * time.Minute,
 	}))
 }
